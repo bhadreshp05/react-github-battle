76b88bc180facf98d6fd2272c6ee801d3e504754
Patel, Bhadresh <bhadresh.patel@yesmail.com>
2017-01-28 15:56:15 -0600

    adding css animation on route change

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom';
+import routes from './config/routes';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import './styles/index.css';
-import routes from './config/routes';
 
 
 ReactDOM.render(
